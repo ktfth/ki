47c07f9886bae5f029079f3b889c287f905b92e0
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-02-12 15:41:47 -0300

    Project started

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Introduction
+//
+// That language are created to be small, flexible and sophisticated
+// with all features like:
+//   - variables context control
+//   - functions, lambdas and pattern matching
+//   - procedures and parallel processing
+//   - modular
+//   - expressive
