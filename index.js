55318a61b4b65d52627037c9f0d60088b179652e
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-02-15 17:48:30 -0300

    Cleaned the tests

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
 'use strict';
-const assert = require('assert');
-
-// Introduction
-//
-// That language are created to be small, flexible and sophisticated
-// with all features like:
-//   - variables context control
-//   - functions, lambdas and pattern matching
-//   - procedures and parallel processing
-//   - modular
-//   - expressive
 
 function tokenizer(input) {
   let current = 0;
@@ -419,83 +408,6 @@ function compiler(input) {
 
   return output;
 }
-
-const input = `
-  let x = 10;
-  print(x);
-`;
-
-const output = `var x = 10;\nconsole.log(x);`;
-
-const tokens = [
-  { type: 'keyword', value: 'let' },
-  { type: 'name', value: 'x' },
-  { type: 'assignment', value: '=' },
-  { type: 'number', value: '10' },
-  { type: 'delimiter', value: ';' },
-  { type: 'keyword', value: 'print' },
-  { type: 'paren', value: '(' },
-  { type: 'name', value: 'x' },
-  { type: 'paren', value: ')' },
-  { type: 'delimiter', value: ';' }
-];
-
-const ast = {
-  type: 'Program',
-  body: [{
-    type: 'AssignmentExpression',
-    name: 'x',
-    value: {
-      type: 'NumberLiteral',
-      value: '10'
-    }
-  }, {
-    type: 'CallExpression',
-    name: 'print',
-    params: [{
-      type: 'Accessment',
-      name: 'x',
-      value: {
-        type: 'NumberLiteral',
-        value: '10'
-      }
-    }]
-  }]
-};
-
-const newAst = {
-  type: 'Program',
-  body: [{
-    type: 'AssignmentStatement',
-    expression: {
-      type: 'AssignmentExpression',
-      register: {
-        type: 'NumberLiteral',
-        name: 'x',
-        value: '10'
-      }
-    }
-  }, {
-    type: 'ExpressionStatement',
-    expression: {
-      type: 'CallExpression',
-      callee: {
-        type: 'Identifier',
-        name: 'print'
-      },
-      arguments: [{
-        type: 'Accessment',
-        value: 'x'
-      }]
-    }
-  }]
-};
-
-assert.deepStrictEqual(tokenizer(input), tokens);
-assert.deepStrictEqual(parser(tokens), ast);
-assert.deepStrictEqual(transformer(ast), newAst);
-assert.deepStrictEqual(codeGenerator(newAst), output);
-assert.deepStrictEqual(compiler(input), output);
 
 module.exports = {
   tokenizer,
