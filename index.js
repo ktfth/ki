62ca7a3b275937c350771b0ba67e691585f47519
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-02-15 13:26:07 -0300

    New ast completed

--- index.js
+++ index.js
@@ -254,8 +254,124 @@ function parser(tokens) {
   return ast;
 }
 
-function transformer(ast, visitor) {
-  return ast;
+function traverser(ast, visitor) {
+  function traverseArray(array, parent) {
+    array.forEach(child => {
+      traverseNode(child, parent);
+    });
+  }
+
+  function traverseNode(node, parent) {
+    let methods = visitor[node.type];
+
+    if (methods && methods.enter) {
+      methods.enter(node, parent);
+    }
+
+    switch (node.type) {
+      case 'Program':
+        traverseArray(node.body, node);
+        break;
+      case 'CallExpression':
+        traverseArray(node.params, node);
+        break;
+      case 'AssignmentExpression':
+      case 'Accessment':
+      case 'NumberLiteral':
+      case 'StringLiteral':
+        break;
+      default:
+        throw new TypeError(node.type);
+    }
+
+    if (methods && methods.exit) {
+      methods.exit(node, parent);
+    }
+  }
+
+  traverseNode(ast, null);
+}
+
+function transformer(ast) {
+  let newAst = {
+    type: 'Program',
+    body: [],
+  };
+
+  ast._context = newAst.body;
+
+  traverser(ast, {
+    Accessment: {
+      enter(node, parent) {
+        parent._context.push({
+          type: 'Accessment',
+          value: node.name,
+        });
+      }
+    },
+
+    NumberLiteral: {
+      enter(node, parent) {
+        parent._context.push({
+          type: 'NumberLiteral',
+          value: node.value,
+        });
+      }
+    },
+
+    StringLiteral: {
+      enter(node, parent) {
+        parent._context.push({
+          type: 'StringLiteral',
+          value: node.value
+        });
+      }
+    },
+
+    CallExpression: {
+      enter(node, parent) {
+        let expression = {
+          type: 'CallExpression',
+          callee: {
+            type: 'Identifier',
+            name: node.name,
+          },
+          arguments: []
+        };
+
+        node._context = expression.arguments;
+
+        if (parent.type !== 'CallExpression') {
+          expression = {
+            type: 'ExpressionStatement',
+            expression: expression,
+          }
+        }
+
+        parent._context.push(expression);
+      }
+    },
+
+    AssignmentExpression: {
+      enter(node, parent) {
+        let expression = {
+          type: 'AssignmentStatement',
+          expression: {
+            type: 'AssignmentExpression',
+            register: {
+              type: node.value.type,
+              name: node.name,
+              value: node.value.value
+            }
+          }
+        };
+
+        parent._context.push(expression);
+      }
+    },
+  });
+
+  return newAst;
 }
 
 const input = `
