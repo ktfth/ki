5b891526531e53602759583312a44142d6e1d70e
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-03-13 15:12:42 -0300

    Covered the division operation

--- index.test.js
+++ index.test.js
@@ -2775,4 +2775,74 @@ describe('Ki', () => {
     assert.deepStrictEqual(codeGenerator(newAst), output);
     assert.deepStrictEqual(compiler(input), output);
   });
+
+  it('should be a division operation', () => {
+    const input = `
+      let kiDiv = 10 / 10;
+    `;
+
+    const output = `var kiDiv = 10 / 10;`;
+
+    const tokens = [
+      { type: 'keyword', value: 'let' },
+      { type: 'name', value: 'kiDiv' },
+      { type: 'assignment', value: '=' },
+      { type: 'number', value: '10' },
+      { type: 'operation', value: '/' },
+      { type: 'number', value: '10' },
+      { type: 'delimiter', value: ';' },
+    ];
+
+    const ast = {
+      type: 'Program',
+      body: [{
+        type: 'AssignmentExpression',
+        name: 'kiDiv',
+        value: {
+          type: 'OperationExpression',
+          operator: '/',
+          values: [{
+            type: 'NumberLiteral',
+            value: '10'
+          }, {
+            type: 'NumberLiteral',
+            value: '10'
+          }]
+        }
+      }]
+    };
+
+    const newAst = {
+      type: 'Program',
+      body: [{
+        type: 'AssignmentStatement',
+        expression: {
+          type: 'AssignmentExpression',
+          register: {
+            name: 'kiDiv',
+            value: {
+              type: 'OperationStatement',
+              expression: {
+                type: 'OperationExpression',
+                operator: '/',
+                values: [{
+                  type: 'NumberLiteral',
+                  value: '10'
+                }, {
+                  type: 'NumberLiteral',
+                  value: '10'
+                }]
+              }
+            }
+          }
+        }
+      }]
+    };
+
+    assert.deepStrictEqual(tokenizer(input), tokens);
+    assert.deepStrictEqual(parser(tokens), ast);
+    assert.deepStrictEqual(transformer(ast), newAst);
+    assert.deepStrictEqual(codeGenerator(newAst), output);
+    assert.deepStrictEqual(compiler(input), output);
+  });
 });
