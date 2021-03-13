ea5b199cb8aa71548353727dbae7b803b71b4ef6
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-03-13 14:53:57 -0300

    Covered the mul operation

--- index.test.js
+++ index.test.js
@@ -2705,4 +2705,74 @@ describe('Ki', () => {
     assert.deepStrictEqual(codeGenerator(newAst), output);
     assert.deepStrictEqual(compiler(input), output);
   });
+
+  it('should be a multiplication operation', () => {
+    const input = `
+      let kiMul = 10 * 10;
+    `;
+
+    const output = `var kiMul = 10 * 10;`;
+
+    const tokens = [
+      { type: 'keyword', value: 'let' },
+      { type: 'name', value: 'kiMul' },
+      { type: 'assignment', value: '=' },
+      { type: 'number', value: '10' },
+      { type: 'operation', value: '*' },
+      { type: 'number', value: '10' },
+      { type: 'delimiter', value: ';' },
+    ];
+
+    const ast = {
+      type: 'Program',
+      body: [{
+        type: 'AssignmentExpression',
+        name: 'kiMul',
+        value: {
+          type: 'OperationExpression',
+          operator: '*',
+          values: [{
+            type: 'NumberLiteral',
+            value: '10'
+          }, {
+            type: 'NumberLiteral',
+            value: '10'
+          }]
+        }
+      }]
+    };
+
+    const newAst = {
+      type: 'Program',
+      body: [{
+        type: 'AssignmentStatement',
+        expression: {
+          type: 'AssignmentExpression',
+          register: {
+            name: 'kiMul',
+            value: {
+              type: 'OperationStatement',
+              expression: {
+                type: 'OperationExpression',
+                operator: '*',
+                values: [{
+                  type: 'NumberLiteral',
+                  value: '10'
+                }, {
+                  type: 'NumberLiteral',
+                  value: '10'
+                }]
+              }
+            }
+          }
+        }
+      }]
+    };
+
+    assert.deepStrictEqual(tokenizer(input), tokens);
+    assert.deepStrictEqual(parser(tokens), ast);
+    assert.deepStrictEqual(transformer(ast), newAst);
+    assert.deepStrictEqual(codeGenerator(newAst), output);
+    assert.deepStrictEqual(compiler(input), output);
+  });
 });
