a4369c33b7ac6537175932dbe4b4bc9128384480
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-03-15 16:21:06 -0300

    Covered all cases for conditional block mutation

--- index.test.js
+++ index.test.js
@@ -3083,5 +3083,6 @@ describe('Ki', () => {
     assert.deepStrictEqual(parser(tokens), ast);
     assert.deepStrictEqual(transformer(ast), newAst);
     assert.deepStrictEqual(codeGenerator(newAst), output);
+    assert.deepStrictEqual(compiler(input), output);
   });
 });
