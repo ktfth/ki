d1119a969e19e12b75108b5921274461054be7ee
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-03-18 17:55:43 -0300

    Covered the not equal statement on transformation

--- index.test.js
+++ index.test.js
@@ -3401,7 +3401,27 @@ describe('Ki', () => {
       }]
     };
 
+    const newAst = {
+      type: 'Program',
+      body: [{
+        type: 'NotEqualStatement',
+        expression: {
+          type: 'NotEqualExpression',
+          value: '!=',
+          leftHand: {
+            type: 'BooleanLiteral',
+            value: 'true'
+          },
+          rightHand: {
+            type: 'BooleanLiteral',
+            value: 'false'
+          }
+        }
+      }]
+    };
+
     assert.deepStrictEqual(tokenizer(input), tokens);
     assert.deepStrictEqual(parser(tokens), ast);
+    assert.deepStrictEqual(transformer(ast), newAst);
   });
 });
