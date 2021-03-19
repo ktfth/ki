c5e7968cb50ce6b233f35ccd6810076db9d3c020
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-03-19 15:33:27 -0300

    Covered the logic or in all compilation process

--- index.test.js
+++ index.test.js
@@ -3632,6 +3632,45 @@ describe('Ki', () => {
       { type: 'delimiter', value: ';' },
     ];
 
+    const ast = {
+      type: 'Program',
+      body: [{
+        type: 'LogicExpression',
+        value: 'or',
+        leftHand: {
+          type: 'BooleanLiteral',
+          value: 'true'
+        },
+        rightHand: {
+          type: 'BooleanLiteral',
+          value: 'false'
+        }
+      }]
+    };
+
+    const newAst = {
+      type: 'Program',
+      body: [{
+        type: 'LogicStatement',
+        expression: {
+          type: 'LogicExpression',
+          value: 'or',
+          leftHand: {
+            type: 'BooleanLiteral',
+            value: 'true'
+          },
+          rightHand: {
+            type: 'BooleanLiteral',
+            value: 'false'
+          }
+        }
+      }]
+    };
+
     assert.deepStrictEqual(tokenizer(input), tokens);
+    assert.deepStrictEqual(parser(tokens), ast);
+    assert.deepStrictEqual(transformer(ast), newAst);
+    assert.deepStrictEqual(codeGenerator(newAst), output);
+    assert.deepStrictEqual(compiler(input), output);
   });
 });
