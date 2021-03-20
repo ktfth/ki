ebb6b505d99e4cede95335f9b2025fdac68e38c4
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-03-20 20:17:10 -0300

    Covered the logic and with multiple options code generation

--- index.test.js
+++ index.test.js
@@ -3747,5 +3747,6 @@ describe('Ki', () => {
     assert.deepStrictEqual(tokenizer(input), tokens);
     assert.deepStrictEqual(parser(tokens), ast);
     assert.deepStrictEqual(transformer(ast), newAst);
+    assert.deepStrictEqual(codeGenerator(newAst), output);
   });
 });
