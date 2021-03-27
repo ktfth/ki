0b7ea91feea41a3952c824eab07667d09f24123f
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-03-27 18:56:02 -0300

    Covered the multiple conditions on the transformer

--- index.test.js
+++ index.test.js
@@ -4920,8 +4920,92 @@ describe('Ki', () => {
       }]
     };
 
+    const newAst = {
+      type: 'Program',
+      body: [{
+        type: 'FunctionStatement',
+        expression: {
+          type: 'FunctionExpression',
+          name: 'fib',
+          params: [{
+            type: 'Argument',
+            value: 'n'
+          }],
+          block: [{
+            type: 'ConditionalStatement',
+            expression: {
+              type: 'ConditionalExpression',
+              name: 'if',
+              conditions: [{
+                type: 'LessThanStatement',
+                expression: {
+                  type: 'LessThanExpression',
+                  value: '<',
+                  leftHand: {
+                    type: 'Accessment',
+                    value: 'n'
+                  },
+                  rightHand: {
+                    type: 'NumberLiteral',
+                    value: '0'
+                  }
+                }
+              }],
+              block: [{
+                type: 'ExpressionStatement',
+                expression: {
+                  type: 'CallExpression',
+                  callee: {
+                    type: 'Identifier',
+                    name: 'print'
+                  },
+                  arguments: [{
+                    type: 'StringLiteral',
+                    value: 'incorrect input'
+                  }]
+                }
+              }]
+            }
+          }, {
+            type: 'ConditionalStatement',
+            expression: {
+              type: 'ConditionalExpression',
+              name: 'elif',
+              conditions: [{
+                type: 'EqualStatement',
+                expression: {
+                  type: 'EqualExpression',
+                  value: '===',
+                  leftHand: {
+                    type: 'Accessment',
+                    value: 'n'
+                  },
+                  rightHand: {
+                    type: 'NumberLiteral',
+                    value: '0'
+                  }
+                }
+              }],
+              block: [{
+                type: 'ReturnStatement',
+                expression: {
+                  type: 'ReturnExpression',
+                  name: 'return',
+                  values: [{
+                    type: 'NumberLiteral',
+                    value: '0'
+                  }]
+                }
+              }]
+            }
+          }]
+        }
+      }]
+    };
+
     assert.deepStrictEqual(tokenizer(input), tokens);
     assert.deepStrictEqual(parser(tokens), ast);
+    assert.deepStrictEqual(transformer(ast), newAst);
   });
 
   // it('should be functional example', () => {
