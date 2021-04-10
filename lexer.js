567e29f0517cadfe0c69a5d22ddf9587628409f9
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-04-10 20:05:31 -0300

    Implemented the object literal and covered on the tests

--- lexer.js
+++ lexer.js
@@ -1,22 +1,67 @@
 'use strict';
 const assert = require('assert');
 
-function lexer(token) {
+function isArray(o) {
+	return o.constructor.toString().indexOf('Array') > -1;
+}
+
+function copy(o) {
+	let out = {};
+	Object.keys(o).forEach(k => out[k] = o[k]);
+	return out;
+}
+
+function lexer(t) {
 	let node = {};
 
-	if (token.type === 'number') {
+	if (!isArray(t) && t.type === 'number') {
 		node.type = 'NumberLiteral';
-		node.value = token.value;
+		node.value = t.value;
 	}
 
-	if (token.type === 'string') {
+	if (!isArray(t) && t.type === 'string') {
 		node.type = 'StringLiteral';
-		node.value = token.value;
+		node.value = t.value;
 	}
 
-	if (token.type === 'boolean') {
+	if (!isArray(t) && t.type === 'boolean') {
 		node.type = 'BooleanLiteral';
-		node.value = token.value;
+		node.value = t.value;
+	}
+
+	if (isArray(t)) {
+		let entry = t[0];
+		if (entry.type === 'block' && entry.value === '{') {
+			node.type = 'ObjectLiteral';
+			node.values = [];
+
+			let prop = {
+				type: 'PropAssignmentExpression',
+				name: '',
+				value: {},
+			};
+
+			let isPropAssignment = false;
+			let isEndOfBlock = false;
+
+			t.slice(1).forEach(v => {
+				if (v.type === 'param') {
+					prop.name = v.value;
+				}
+
+				if (v.type === 'colon' && v.value === ':') {
+					isPropAssignment = true;
+				}
+
+				if (isPropAssignment && v.type !== 'colon' && v.value !== ':') {
+					prop.value = lexer(v);
+					node.values.push(copy(prop));
+					prop.name = '';
+					prop.value = {};
+					isPropAssignment = false;
+				}
+			});
+		}
 	}
 
 	return node;
@@ -44,4 +89,44 @@ assert.deepStrictEqual(lexer({
 }), {
 	type: 'BooleanLiteral',
 	value: 'true'
+});
+
+assert.deepStrictEqual(lexer([
+	{ type: 'block', value: '{' },
+	{ type: 'param', value: 'a' },
+	{ type: 'colon', value: ':' },
+	{ type: 'number', value: '1' },
+	{ type: 'comma', value: ',' },
+	{ type: 'param', value: 'b' },
+	{ type: 'colon', value: ':' },
+	{ type: 'number', value: '2' },
+	{ type: 'comma', value: ',' },
+	{ type: 'param', value: 'c' },
+	{ type: 'colon', value: ':' },
+	{ type: 'number', value: '3' },
+	{ type: 'block', value: '}' },
+]), {
+	type: 'ObjectLiteral',
+	values: [{
+		type: 'PropAssignmentExpression',
+		name: 'a',
+		value: {
+			type: 'NumberLiteral',
+			value: '1'
+		}
+	}, {
+		type: 'PropAssignmentExpression',
+		name: 'b',
+		value: {
+			type: 'NumberLiteral',
+			value: '2'
+		}
+	}, {
+		type: 'PropAssignmentExpression',
+		name: 'c',
+		value: {
+			type: 'NumberLiteral',
+			value: '3'
+		}
+	}]
 });
