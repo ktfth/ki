cbf4fc42a4d4db068eba2884f7821629c973e965
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-04-10 18:09:39 -0300

    Supporting string literal on the lexer

--- lexer.js
+++ lexer.js
@@ -9,12 +9,26 @@ function lexer(token) {
 		node.value = token.value;
 	}
 
+	if (token.type === 'string') {
+		node.type = 'StringLiteral';
+		node.value = token.value;
+	}
+
 	return node;
 }
 
 assert.deepStrictEqual(lexer({
-	type: 'number', value: '1'
+	type: 'number',
+	value: '1',
 }), {
 	type: 'NumberLiteral',
 	value: '1'
+});
+
+assert.deepStrictEqual(lexer({
+	type: 'string',
+	value: 'ki'
+}), {
+	type: 'StringLiteral',
+	value: 'ki'
 });
