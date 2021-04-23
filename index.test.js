041c9c36a9a61a17a7470f61b196f4611ea0fb5b
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-04-23 12:54:29 -0300

    All the compilation process made for string

--- index.test.js
+++ index.test.js
@@ -18,6 +18,26 @@ describe('Ki', () => {
 			{ type: 'string', value: 'hello world' }
 		];
 
+		const ast = {
+			type: 'Program',
+			body: [{
+				type: 'StringLiteral',
+				value: 'hello world'
+			}]
+		};
+
+		const newAst = {
+			type: 'Program',
+			body: [{
+				type: 'StringLiteral',
+				value: 'hello world'
+			}]
+		};
+
 		assert.deepStrictEqual(tokenizer(input), tokens);
+		assert.deepStrictEqual(parser(tokens), ast);
+		assert.deepStrictEqual(transformer(ast), newAst);
+		assert.deepStrictEqual(codeGenerator(newAst), output);
+		assert.deepStrictEqual(compiler(input), output);
 	});
 });
