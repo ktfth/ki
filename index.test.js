50b9d3f3aacfaf5cf8583ca602df1eb3710f06bd
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-04-24 16:49:58 -0300

    All the compilation made for multiple nested operation expressions

--- index.test.js
+++ index.test.js
@@ -541,5 +541,7 @@ describe('Ki', () => {
 		assert.deepStrictEqual(tokenizer(input), tokens);
 		assert.deepStrictEqual(parser(tokens), ast);
 		assert.deepStrictEqual(transformer(ast), newAst);
+		assert.deepStrictEqual(codeGenerator(newAst), output);
+		assert.deepStrictEqual(compiler(input), output);
 	});
 });
