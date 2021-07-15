4a3a09d1feee919d1d1f3baf323c0d0976b92b45
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-07-15 13:09:29 -0300

    Covering the basic operation for tokenization

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,23 +2,21 @@ const assert = require('assert');
 const { Tokenizer } = require('../');
 
 describe('Tokenizer', () => {
+	let tokenizer = new Tokenizer('1 + 1');
+
 	it('should have an input', () => {
-		let tokenizer = new Tokenizer('1 + 1');
 		assert.equal(tokenizer.input, '1 + 1');
 	});
 
 	it('should have a current pointer', () => {
-		let tokenizer = new Tokenizer('1 + 1');
 		assert.equal(tokenizer.current, 0);
 	});
 
 	it('should have tokens', () => {
-		let tokenizer = new Tokenizer('1 + 1');
 		assert.deepEqual(tokenizer.tokens, []);
 	});
 
-	it('should have a mechanism', () => {
-		let tokenizer = new Tokenizer('1 + 1');
+	it('should have a mechanism for number', () => {
 		let interaction = (char, tokens, current, input) => {
 			let NUMBERS = /[0-9]/;
 			if (NUMBERS.test(char)) {
@@ -37,10 +35,47 @@ describe('Tokenizer', () => {
 				tokens,
 				current,
 				input,
-				continue: true
+				continue: true,
 			};
 		};
 		tokenizer.mechanism['number'] = interaction;
-		assert.deepEqual(tokenizer.mechanism, { number: interaction });
+		assert.deepEqual(tokenizer.mechanism['number'], interaction);
+	});
+
+	it('should have a mechanism for space or break line', () => {
+		let interaction = (char, tokens, current, input) => {
+			if (/\n|\s/.test(char)) {
+				current++;
+			}
+
+			return {
+				char,
+				tokens,
+				current,
+				input,
+				continue: true,
+			};
+		};
+		tokenizer.mechanism['special'] = interaction;
+		assert.deepEqual(tokenizer.mechanism['special'], interaction);
+	});
+
+	it('should have a mechanism for plus operation', () => {
+		let interaction = (char, tokens, current, input) => {
+			if (char === '+') {
+				tokens.push({ type: 'operation', value: char });
+				current++;
+			}
+
+			return {
+				char,
+				tokens,
+				current,
+				input,
+				continue: true
+			};
+		};
+		tokenizer.mechanism['sum'] = interaction;
+		assert.deepEqual(tokenizer.mechanism['sum'], interaction);
 	});
 });
