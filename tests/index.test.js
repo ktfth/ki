dedd3d34e30c7d1aacfafbe00ab29236b1006063
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-07-14 21:27:10 -0300

    Bare structure of mechanism on tokenizer

--- tests/index.test.js
+++ tests/index.test.js
@@ -15,5 +15,31 @@ describe('Tokenizer', () => {
 	it('should have tokens', () => {
 		let tokenizer = new Tokenizer('1 + 1');
 		assert.deepEqual(tokenizer.tokens, []);
-	})
+	});
+
+	it('should have a mechanism', () => {
+		let tokenizer = new Tokenizer('1 + 1');
+		let interaction = (char, tokens, current, input) => {
+			let NUMBERS = /[0-9]/;
+			if (NUMBERS.test(char)) {
+				let value = '';
+
+				while (NUMBERS.test(char)) {
+					value += char;
+					char = input[++current];
+				}
+			}
+
+			tokens.push({ type: 'number', value });
+
+			return {
+				char,
+				tokens,
+				current,
+				input
+			};
+		};
+		tokenizer.mechanism['number'] = interaction;
+		assert.deepEqual(tokenizer.mechanism, { number: interaction });
+	});
 });
