78ebae88a56f428c05a2f99b1e539346d127a651
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-07-15 21:51:49 -0300

    Included the current pointer

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,16 +2,21 @@ const assert = require('assert');
 const { Parser } = require('../');
 
 describe('Parser', () => {
+	let parser = new Parser([
+		{ type: 'number', value: '1' },
+		{ type: 'operation', value: '+' },
+		{ type: 'number', value: '1' },
+	]);
+
 	it('should have tokens', () => {
-		let parser = new Parser([
-			{ type: 'number', value: '1' },
-			{ type: 'operation', value: '+' },
-			{ type: 'number', value: '1' },
-		]);
 		assert.deepEqual(parser.tokens, [
 			{ type: 'number', value: '1' },
 			{ type: 'operation', value: '+' },
 			{ type: 'number', value: '1' },
 		]);
+	});
+
+	it('should have current', () => {
+		assert.equal(parser.current, 0);
 	});
 });
