c150305ac98fdb02d5751d078990ee62f0176eb5
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-07-18 20:15:07 -0300

    Changed the code generator tests for a specific file Implemented the compiler

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const Tokenizer = require('./lib/tokenizer');
 const Parser = require('./lib/parser');
 const Traverser = require('./lib/traverser');
 const CodeGenerator = require('./lib/code-generator');
+const Compiler = require('./lib/compiler');
 
 module.exports = {
 	Tokenizer,
 	Parser,
 	Traverser,
 	CodeGenerator,
+	Compiler,
 };
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,68 +1,10 @@
 const assert = require('assert');
-const { CodeGenerator } = require('../');
+const { Compiler } = require('../');
 
-describe('CodeGenerator', () => {
-	let codeGenerator = new CodeGenerator({
-		type: 'Program',
-		body: [
-			{ type: 'NumberLiteral', value: '1' },
-			{ type: 'Operation', value: '+' },
-			{ type: 'NumberLiteral', value: '1' },
-		]
-	});
-	let codeGeneratorThrows = new CodeGenerator({
-		type: 'Program',
-		body: [
-			{ type: 'Assignment', value: '=' },
-		]
-	});
+describe('Compiler', () => {
+	let compiler = new Compiler('1 + 1');
 
-	it('should have node', () => {
-		assert.deepEqual(codeGenerator.node, {
-			type: 'Program',
-			body: [
-				{ type: 'NumberLiteral', value: '1' },
-				{ type: 'Operation', value: '+' },
-				{ type: 'NumberLiteral', value: '1' },
-			]
-		});
-	});
-
-	it('should have a mechanism for program', () => {
-		let interactionProgram = (node) => {
-			return node.body.map(codeGenerator.run).join('');
-		};
-		codeGenerator.mechanism['Program'] = interactionProgram;
-		codeGeneratorThrows.mechanism['Program'] = interactionProgram;
-		assert.deepEqual(codeGenerator.mechanism['Program'], interactionProgram);
-	});
-
-	it('should have a mechanism for number literal', () => {
-		let interactionNumberLiteral = (node) => {
-			return '' + node.value + ' ';
-		};
-		codeGenerator.mechanism['NumberLiteral'] = interactionNumberLiteral;
-		codeGeneratorThrows.mechanism['NumberLiteral'] = interactionNumberLiteral;
-		assert.deepEqual(codeGenerator.mechanism['NumberLiteral'], interactionNumberLiteral);
-	});
-
-	it('should have a mechanism for operation', () => {
-		let interactionOperation = (node) => {
-			return '' + node.value + ' ';
-		};
-		codeGenerator.mechanism['Operation'] = interactionOperation;
-		codeGeneratorThrows.mechanism['Operation'] = interactionOperation;
-		assert.deepEqual(codeGenerator.mechanism['Operation'], interactionOperation);
-	});
-
-	it('should run code generator', () => {
-		codeGenerator.run();
-		assert.equal(codeGenerator.output, '1 + 1');
-	});
-
-	it('should run code generator and throws a type error', () => {
-		assert.throws(() => {
-			codeGeneratorThrows.run();
-		});
+	it('should have an input', () => {
+		assert.equal(compiler.input, '1 + 1');
 	});
 });
