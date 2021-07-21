35e34bdd4aa5dbe5e5672e8d509645a564e98a9e
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-07-21 17:51:52 -0300

    Supporting terminations

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const { Compiler } = require('../');
 
-describe('Compiler', () => {
+describe('Compiler basic instructions', () => {
 	let compiler = new Compiler('1 + 1');
 
 	it('should have an input', () => {
@@ -11,5 +11,18 @@ describe('Compiler', () => {
 	it('should have an output', () => {
 		compiler.run();
 		assert.equal(compiler.output, '1 + 1');
+	});
+});
+
+describe('Compiler terminations', () => {
+	let compiler = new Compiler('3 * 3;');
+
+	it('should have an input', () => {
+		assert.equal(compiler.input, '3 * 3;');
+	});
+
+	it('should have an output', () => {
+		compiler.run();
+		assert.equal(compiler.output, '3 * 3;');
 	});
 });
